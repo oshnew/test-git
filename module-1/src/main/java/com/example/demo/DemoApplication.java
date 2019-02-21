1d59e0b56dfd7d9e96deceee09f13fabffd6aada
Seung-Ha Eom <oshnew@naver.com>
2019-02-21 15:34:57 +0900

    module 1 commit

---
+++ module-1/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+
+		System.out.println("\n\n == 서버 시작 완료 됨");
+	}
+
+}
